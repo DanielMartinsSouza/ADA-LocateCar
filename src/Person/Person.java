1cbe029b2d75fdee738b7cdeb99076db69d7d1bb
DanielMartinsSouza <danielmartsouza@gmail.com>
2024-03-12 13:29:32 -0300

    FEAT: Creating class Person

---
+++ src/Person/Person.java
@@ -0,0 +1,27 @@
+package Person;
+
+public abstract class Person {
+    private String documento;
+    private String nome;
+
+    public Person(String documento, String nome) {
+        this.documento = documento;
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
