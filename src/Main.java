253047320bbe23a965ceaef6e5c3cac8d6994816
MBorgesM <45786822+MBorgesM@users.noreply.github.com>
2024-03-14 01:50:13 -0300

    Removendo código de teste

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import utils.menu.*;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 
@@ -43,21 +40,6 @@ public class Main
 
         MenuPrincipal.exitMessage();
         sc.close();
-
-        LocalDateTime data = LocalDateTime.now();
-        LocalDateTime dataLogoAposAluguel = data.plusMinutes(15);
-        LocalDateTime dataTresDiasAntes = data.minusDays(3);
-        LocalDateTime dataDoisDiasAntes = dataTresDiasAntes.plusMinutes(1);
-        long tresDias = Duration.between(dataTresDiasAntes, data).toDays() + 1;
-        long doisDias = Duration.between(dataDoisDiasAntes, data).toDays() + 1;
-        long nenhumDia = Duration.between(dataLogoAposAluguel, data).toDays() + 1;
-        System.out.println(data);
-        System.out.println(dataLogoAposAluguel);
-        System.out.println(dataTresDiasAntes);
-        System.out.println(dataDoisDiasAntes);
-        System.out.println(nenhumDia);
-        System.out.println(tresDias);
-        System.out.println(doisDias);
     }
 
     private static void opcoesAluguel(Scanner sc) {
